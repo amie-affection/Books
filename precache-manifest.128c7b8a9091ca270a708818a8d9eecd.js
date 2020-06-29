493f96698ecec4ad0ec49a4e3be7dff68d384c26
amie-affection <amie.affection@gmail.com>
2020-06-29 14:22:09 +0300

    Updates

---
+++ precache-manifest.128c7b8a9091ca270a708818a8d9eecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25967b50660db945dc835a90c0e0b14c",
+    "url": "/Books/index.html"
+  },
+  {
+    "revision": "c2eb14322ec56c2e07f7",
+    "url": "/Books/static/js/2.0b1d0b37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Books/static/js/2.0b1d0b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90eca5abfe5ae520e344",
+    "url": "/Books/static/js/main.b04db20a.chunk.js"
+  },
+  {
+    "revision": "09cee31a3371749b40f1",
+    "url": "/Books/static/js/runtime-main.d2d9c57d.js"
+  }
+]);
